5e37687541c2674e56131bbacaefe3ce125c2a8e
Dmitrii Posadskii <dmitry.posadsky@gmail.com>
2020-01-19 23:38:25 +0300

    Template page file for page and left part of page

---
+++ src/component/LeftPage.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+class LeftPage extends Component {
+	render() {
+		const {
+			pageName,
+		} = this.props;
+
+		return (
+			<div style={{flex: 1, paddingLeft: "40px", paddingRight: "20px", display: "flex", flexDirection: "column"}}>
+				<div style={{flex: 1, color: "white", display: "flex", flexDirection: "column", justifyContent: "center", fontFamily: "Fira Mono", fontWeight: 900, fontSize: "25px", opacity: 0.3}}>
+					<p style={{margin: 0}}>public class <p style={{display: "initial", color: "#00B5AD", opacity: 1}}>{pageName}</p> {'{'}</p>
+					<p style={{paddingLeft: "20px", margin: 0}}>public String toString() {'{'}</p>
+					<p style={{paddingLeft: "40px", margin: 0}}>return "</p>
+				</div>
+				<div style={{flex: 1, paddingLeft: "60px", color: "white", display: "flex", flexDirection: "column", justifyContent: "center", fontFamily: "Fira Mono", fontSize: "20px"}}>
+					{this.props.children}
+				</div>
+				<div style={{flex: 1, color: "white", display: "flex", flexDirection: "column", justifyContent: "center", fontFamily: "Fira Mono", fontWeight: 900, fontSize: "25px", opacity: 0.3}}>
+					<p style={{paddingLeft: "40px", margin: 0}}>";</p>
+					<p style={{paddingLeft: "20px", margin: 0}}>{'}'}</p>
+					<p style={{margin: 0}}>{'}'}</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default LeftPage;
---
+++ src/component/index.js
@@ -0,0 +1,7 @@
+import LeftRightPage from './LeftRightPage';
+import LeftPage from './LeftPage';
+
+export {
+	LeftRightPage,
+	LeftPage,
+};
