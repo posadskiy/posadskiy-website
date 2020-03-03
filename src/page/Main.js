d7cc548b7c6ea64fe6a283dd1fcebe7860d61cec
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-03-03 21:47:24 +0300

    Replace main photo

--- src/page/Main.js
+++ src/page/Main.js
@@ -8,13 +8,13 @@ class Main extends Component {
 				<div id="hello" style={{flex: 1, background: "#1d1d1d", paddingLeft: "40px", alignSelf: "center"}}>
 					<div style={{textAlign: "left"}}>
 						<p style={{margin: 0, color: "white", fontSize: "40px", fontWeight: 900}}>Hi,</p>
-						<p style={{margin: 0, color: "white", fontSize: "40px", fontWeight: 900}}>I'm Posadskiy</p>
+						<p style={{margin: 0, color: "white", fontSize: "40px", fontWeight: 900}}>I'm Dimitri Posadskiy</p>
 						<p style={{margin: 0, color: "white", fontSize: "40px", fontWeight: 900}}>full stack developer</p>
 						<p style={{margin: 0, color: "white", fontSize: "20px", fontWeight: 700}}>Java / JavaScript / React / React Native / Big Data / Machine Learning</p>
 					</div>
 				</div>
-				<div id="photo" style={{alignSelf: "center", paddingRight: "40px", flex: 1, textAlign: "-webkit-right"}}>
-					<Image src='/main_photo.png' size="large" />
+				<div id="photo" style={{alignSelf: "center", height: "100%", paddingRight: "40px", flex: 1, textAlign: "-webkit-right"}}>
+					<Image src='/main_photo.png' style={{height: "100%"}} />
 				</div>
 			</div>
 		)
