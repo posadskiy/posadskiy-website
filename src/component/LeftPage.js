95c1ea3376c23c69c4c642b423abea41830de104
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-03-05 23:30:21 +0300

    Fix <p/> with inner <p/> bug

--- src/component/LeftPage.js
+++ src/component/LeftPage.js
@@ -9,7 +9,7 @@ class LeftPage extends Component {
 		return (
 			<div style={{flex: 1, paddingLeft: "40px", paddingRight: "20px", display: "flex", flexDirection: "column"}}>
 				<div style={{flex: 1, color: "white", display: "flex", flexDirection: "column", justifyContent: "center", fontFamily: "Fira Mono", fontWeight: 900, fontSize: "25px", opacity: 0.3}}>
-					<p style={{margin: 0}}>public class <p style={{display: "initial", color: "#00B5AD", opacity: 1}}>{pageName}</p> {'{'}</p>
+					<p style={{margin: 0}}>public class <span style={{display: "initial", color: "#00B5AD", opacity: 1}}>{pageName}</span> {'{'}</p>
 					<p style={{paddingLeft: "20px", margin: 0}}>public String toString() {'{'}</p>
 					<p style={{paddingLeft: "40px", margin: 0}}>return "</p>
 				</div>
