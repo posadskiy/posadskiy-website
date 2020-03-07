db667fea2e26940e5b57e26e95a029b510408fe7
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-03-07 19:10:39 +0300

    Add page scroll for mobile version

--- src/component/LeftRightPage.js
+++ src/component/LeftRightPage.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class LeftRightPage extends Component {
 	render() {
 		return (
-			<div style={{display: "flex", width: "100%"}}>
+			<div style={{display: "flex", width: "100%", flexWrap: "wrap", overflow: "scroll"}}>
 				{this.props.children}
 			</div>
 		)
