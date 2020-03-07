f7bbf83b9732b68963313a4eae6f27e4ded4efc7
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2020-03-07 19:08:40 +0300

    Make Projects background lightgray for better visibility

--- src/component/ProjectItem.js
+++ src/component/ProjectItem.js
@@ -11,13 +11,13 @@ class ProjectItem extends Component {
 
 		return (
 			<Reveal animated='move up' style={{display: "flex", width: "100%", height: "100%"}}>
-				<Reveal.Content visible style={{width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", background: "white"}}>
-					<Image style={{background: "white", maxWidth: "70%", maxHeight: "70%", alignSelf: "center"}} src={image} />
+				<Reveal.Content visible style={{width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", background: "lightgray"}}>
+					<Image style={{maxWidth: "70%", maxHeight: "70%", alignSelf: "center"}} src={image} />
 				</Reveal.Content>
 				<Reveal.Content hidden style={{alignSelf: "center"}}>
 					<p style={{fontWeight: 900, margin: 0, textAlign: "center"}}>{header}</p>
 					<p style={{textAlign: "center", marginBottom: "5px"}}>{description}</p>
-					<Button basic color="#00B5AD" onClick={() => console.log(111)} style={{width: "100%"}}>Learn more</Button>
+					<Button basic color="teal" onClick={() => console.log(111)} style={{width: "100%"}}>Learn more</Button>
 				</Reveal.Content>
 			</Reveal>
 		)
