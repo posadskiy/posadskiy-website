8ce9b3d2daff28ed748b60e997db3414abec4452
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2021-08-30 19:03:12 +0300

    Improve text on Projects page

--- src/page/desktop/Projects.js
+++ src/page/desktop/Projects.js
@@ -7,15 +7,16 @@ class Projects extends Component {
 		return (
 			<Desktop.LeftRightPage>
 				<Desktop.LeftPage pageName="Projects">
-					<p>My projects are divided into three types.</p>
-					<p>At <span style={{color: "#00B5AD"}}>day job</span>, I am based on business priorities. I select a solution
-						and technology for a specific task.</p>
-					<p>I develop <span style={{color: "#00B5AD"}}>solutions that help</span> my family in free time. These
-						solutions automate everyday processes, such as financial accounting or skills training. Here I try new
-						technologies. The code and setup are always publicly available - anyone can use it for themselves.</p>
-					<p>My <span style={{color: "#00B5AD"}}>Open Source libraries</span> are self-contained components that
-						appeared during development. If I find a way to make the library better, I’m releasing an update. Of course,
-						I fix bugs found by users and provide support.</p>
+					<p>I'm moving forward on several fronts.</p>
+					<p>At <span style={{color: "#00B5AD"}}>day job</span>, I base on business priorities. My motto is that 
+            technology should be selected for a specific task, but not using one technology for all tasks.</p>
+					<p>In my free time, I develop <span style={{color: "#00B5AD"}}>solutions that help my family</span>. These 
+            solutions as applications, sites, or even 
+            scripts automate everyday processes, such as financial accounting, budget planning, or skills training. 
+            During this activity, I operate with unknown technologies, trying something new. The code and setup are 
+            always publicly available - anyone can use it for themselves.</p>
+					<p>My <span style={{color: "#00B5AD"}}>Open Source libraries</span> are self-contained components that appear 
+            during development, prepares for publishing, maintains, and improves during their life cycle.</p>
 				</Desktop.LeftPage>
 				<Desktop.RightPage pageName="Projects">
 					<Grid columns={3} divided style={{margin: 0, background: "lightgray"}}>
